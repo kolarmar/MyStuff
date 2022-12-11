d59f79a1b986592bce4c87578883e930df5b7ac0
kolarmar <117761052+kolarmar@users.noreply.github.com>
2022-12-11 19:43:13 +0100

    Update Sorts
    Added an ugly comparison between the sorts.

--- C#/Sorts/Sorts/Program.cs
+++ C#/Sorts/Sorts/Program.cs
@@ -1,5 +1,5 @@
-﻿using System.Diagnostics;
-using System.Net.Http.Headers;
+﻿using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Sorts
@@ -29,7 +29,7 @@ namespace Sorts
         {
             int min = arr[i];
             int minIndex = i;
-            for (int j = i; j < arr.Length; j++)
+            for (int j = i;j < arr.Length; j++)
             {
                 if (arr[j] < min)
                 {
@@ -47,7 +47,22 @@ namespace Sorts
             e2 = temp;
         }
 
-        static int[] BubbleSort(int[] arr)
+        static int[] BubbleSortV1(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Swap(ref arr[j], ref arr[j + 1]);
+                    }
+                }
+            }
+            return arr;
+        }
+
+        static int[] BubbleSortV2(int[] arr)
         {
             for (int i = 0; i < arr.Length - 1; i++)
             {
@@ -215,43 +230,441 @@ namespace Sorts
             }
             return arr;
         }
+    
 
-        static void CompareSorts(int[] arr, int left,int right)
+        static TimeSpan Sort_MeasureSelect(int[] arr)
         {
-            Stopwatch stopwatch= new Stopwatch();
-            string SortUsed;
-            int[] selectArr, insertArr, quickArr;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            SelectSort(arr);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
 
-            // SELECT SORT
-            SortUsed = "select";
-            selectArr = arr.ToArray();
-            stopwatch.Start(); SelectSort(selectArr); stopwatch.Stop();
-            Console.WriteLine("Time elapsed for {0} sort: {1}", SortUsed, stopwatch.Elapsed);
-            stopwatch.Reset();
+        static TimeSpan Sort_MeasureQuick(int[] arr)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            QuickSort(arr, 0, arr.Length - 1);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
 
-            // INSERT SORT
-            SortUsed = "insert";
-            insertArr = arr.ToArray();
-            stopwatch.Start(); InsertSort(insertArr); stopwatch.Stop();
-            Console.WriteLine("Time elapsed for {0} sort: {1}", SortUsed, stopwatch.Elapsed);
-            stopwatch.Reset();
+        static TimeSpan Sort_MeasureInsert(int[] arr)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            InsertSort(arr);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
 
-            // QUICK SORT
-            SortUsed = "quick";
-            quickArr = arr.ToArray();
-            stopwatch.Start(); QuickSort(quickArr, left, right); stopwatch.Stop();
-            Console.WriteLine("Time elapsed for {0} sort: {1}", SortUsed, stopwatch.Elapsed);
-            stopwatch.Reset();
+        static TimeSpan Sort_MeasureBubbleV1(int[] arr)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            BubbleSortV1(arr);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        static TimeSpan Sort_MeasureBubbleV2(int[] arr)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            BubbleSortV2(arr);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        static TimeSpan Sort_MeasureShaker(int[] arr)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            ShakerSort(arr);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        static TimeSpan Sort_MeasureMerge(int[] arr)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            MergeSort(arr, 0, arr.Length - 1);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+
+
+        static void SwapMeasure(ref int e1, ref int e2, ref int swapTimes)
+        {
+            int temp = e1;
+            e1 = e2;
+            e2 = temp;
+            swapTimes++;
+        }
+
+        static int BubbleSortV1Swap(int[] arr)
+        {
+            int swapTimes = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        SwapMeasure(ref arr[j], ref arr[j + 1], ref swapTimes);
+                    }
+                }
+            }
+            return swapTimes;
+        }
+
+        static int BubbleSortV2Swap(int[] arr)
+        {
+            int swapTimes = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                bool swapReuqired = false;
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        SwapMeasure(ref arr[j], ref arr[j + 1], ref swapTimes);
+                        swapReuqired = true;
+                    }
+                }
+                if (swapReuqired == false)
+                {
+                    break;
+                }
+            }
+            return swapTimes;
+        }
+
+        static int ShakerSortSwap(int[] arr)
+        {
+            int swapTimes = 0;
+            bool swapRequired;
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                swapRequired = false;
+                for (int j = i; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        SwapMeasure(ref arr[j], ref arr[j + 1], ref swapTimes);
+                        swapRequired = true;
+                    }
+                }
+                if (swapRequired == false)
+                {
+                    break;
+                }
+                swapRequired = false;
+                for (int j = arr.Length - i - 2; j > i; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        SwapMeasure(ref arr[j], ref arr[j + 1], ref swapTimes);
+                        swapRequired = true;
+                    }
+                }
+                if (swapRequired == false)
+                {
+                    break;
+                }
+            }
+            return swapTimes;
+        }
+
+        static int InsertSortSwap(int[] arr)
+        {
+            int swapTimes = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        SwapMeasure(ref arr[j - 1], ref arr[j], ref swapTimes);
+                    }
+                }
+            }
+            return swapTimes;
+        }
+
+        static int SelectSortSwap(int[] arr)
+        {
+            int swapTimes = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minIndex = FindMinIndex(arr, i);
+                SwapMeasure(ref arr[i], ref arr[minIndex], ref swapTimes);
+            }
+            return swapTimes;
+        }
+
+        static int QuickSortSwap(int[] arr, int leftIndex, int rightIndex, int swapTimes)
+        {
+            int i = leftIndex;
+            int j = rightIndex;
+            int pivot = arr[leftIndex];
+
+            while (i <= j)
+            {
+                while (arr[i] < pivot)
+                {
+                    i++;
+                }
+                while (pivot < arr[j])
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    SwapMeasure(ref arr[i], ref arr[j], ref swapTimes);
+                    i++;
+                    j--;
+                }
+            }
+            if (leftIndex < j)
+            {
+                return QuickSortSwap(arr, leftIndex, j, swapTimes);
+            }
+            if (i < rightIndex)
+            {
+                return QuickSortSwap(arr, i, rightIndex, swapTimes);
+            }
+            return swapTimes;
+        }
+
+
+        static int BubbleSortV1_CompareSum(int[] arr)
+        {
+            int compared = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    compared++;
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Swap(ref arr[j], ref arr[j + 1]);
+                    }
+                }
+            }
+            return compared;
+        }
+
+        static int BubbleSortV2_CompareSum(int[] arr)
+        {
+            int compared = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                bool swapReuqired = false;
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    compared++;
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Swap(ref arr[j], ref arr[j + 1]);
+                        swapReuqired = true;
+                    }
+                }
+                if (swapReuqired == false)
+                {
+                    break;
+                }
+            }
+            return compared;
+        }
+
+
+        static int ShakerSort_CompareSum(int[] arr)
+        {
+            bool swapRequired;
+            int compared = 0;
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                swapRequired = false;
+                for (int j = i; j < arr.Length - i - 1; j++)
+                {
+                    compared++;
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Swap(ref arr[j], ref arr[j + 1]);
+                        swapRequired = true;
+                    }
+                }
+                if (swapRequired == false)
+                {
+                    break;
+                }
+                swapRequired = false;
+                for (int j = arr.Length - i - 2; j > i; j--)
+                {
+                    compared++;
+                    if (arr[j - 1] > arr[j])
+                    {
+                        Swap(ref arr[j - 1], ref arr[j]);
+                        swapRequired = true;
+                    }
+                }
+                if (swapRequired == false)
+                {
+                    break;
+                }
+            }
+            return compared;
+        }
+
+        static int InsertSort_CompareSum(int[] arr)
+        {
+            int compared = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    compared++;
+                    if (arr[j - 1] > arr[j])
+                    {
+                        Swap(ref arr[j - 1], ref arr[j]);
+                    }
+                }
+            }
+            return compared;
+        }
+
+        static int SelectSort_CompareSum(int[] arr)
+        {
+            int compared = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int min = arr[i];
+                int minIndex = i;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    compared++;
+                    if (arr[j] < min)
+                    {
+                        min = arr[j];
+                        minIndex = j;
+                    }
+                }
+                Swap(ref arr[i], ref arr[minIndex]);
+            }
+            return compared;
+        }
+
+
+        static int QuickSort_CompareSum(int[] arr, int leftIndex, int rightIndex, int compared)
+        {
+            int i = leftIndex;
+            int j = rightIndex;
+            int pivot = arr[leftIndex];
+
+            while (i <= j)
+            {
+                while (arr[i] < pivot)
+                {
+                    i++;
+                }
+                while (pivot < arr[j])
+                {
+                    j--;
+                }
+                compared++;
+                if (i <= j)
+                {
+                    Swap(ref arr[i], ref arr[j]);
+                    i++;
+                    j--;
+                }
+            }
+            compared++;
+            if (leftIndex < j)
+            {
+                return QuickSort_CompareSum(arr, leftIndex, j, compared);
+            }
+            compared++;
+            if (i < rightIndex)
+            {
+                return QuickSort_CompareSum(arr, i, rightIndex, compared);
+            }
+            return compared;
+        }
+
+
+        static void Sort_CompareTime(int[] array)
+        {
+            // Make copies of master array
+            int[] a = array.ToArray();
+            int[] b = array.ToArray();
+            int[] c = array.ToArray();
+            int[] d = array.ToArray();
+            int[] e = array.ToArray();
+            int[] f = array.ToArray();
+            int[] g = array.ToArray();
+
+            Console.WriteLine("Time taken with SELECT sort: " + Sort_MeasureSelect(a));
+            Console.WriteLine("Time taken with QUICK sort: " + Sort_MeasureQuick(b));
+            Console.WriteLine("Time taken with INSERT sort: " + Sort_MeasureInsert(c));
+            Console.WriteLine("Time taken with BUBBLE V1 sort: " + Sort_MeasureBubbleV1(d));
+            Console.WriteLine("Time taken with BUBBLE V2 sort: " + Sort_MeasureBubbleV2(e));
+            Console.WriteLine("Time taken with SHAKER sort: " + Sort_MeasureShaker(f));
+            Console.WriteLine("Time taken with MERGE sort: " + Sort_MeasureMerge(g));
+        }
+
+        static void Sort_CompareComparisons(int[] array)
+        {
+            // Make copies of master array
+            int[] a = array.ToArray();
+            int[] b = array.ToArray();
+            int[] c = array.ToArray();
+            int[] d = array.ToArray();
+            int[] e = array.ToArray();
+            int[] f = array.ToArray();
+            int[] g = array.ToArray();
+
+            Console.WriteLine("Comparisons made with SELECT sort: " + SelectSort_CompareSum(a));
+            Console.WriteLine("Comparisons made with QUICK sort: " + QuickSort_CompareSum(b, 0, array.Length - 1, 0));
+            Console.WriteLine("Comparisons made with INSERT sort: " + InsertSort_CompareSum(c));
+            Console.WriteLine("Comparisons made with BUBBLE V1 sort: " + BubbleSortV1_CompareSum(d));
+            Console.WriteLine("Comparisons made with BUBBLE V2 sort: " + BubbleSortV2_CompareSum(e));
+            Console.WriteLine("Comparisons made with SHAKER sort: " + ShakerSort_CompareSum(f));
+        }
+
+        static void Sort_CompareSwaps(int[] array)
+        {
+            // Make copies of master array
+            int[] a = array.ToArray();
+            int[] b = array.ToArray();
+            int[] c = array.ToArray();
+            int[] d = array.ToArray();
+            int[] e = array.ToArray();
+            int[] f = array.ToArray();
+
+            Console.WriteLine("Swaps needed for SELECT sort: " + SelectSortSwap(a));
+            Console.WriteLine("Swaps needed for QUICK sort: " + QuickSortSwap(b, 0, array.Length - 1, 0));
+            Console.WriteLine("Swaps needed for INSERT sort: " + InsertSortSwap(c));
+            Console.WriteLine("Swaps needed for BUBBLE V1 sort: " + BubbleSortV1Swap(d));
+            Console.WriteLine("Swaps needed for BUBBLE V2 sort: " + BubbleSortV2Swap(e));
+            Console.WriteLine("Swaps needed for SHAKER sort: " + ShakerSortSwap(f));
+
 
         }
 
+
         static void Main(string[] args)
         {
-            int[] array = RandomArray(2000, 10000);
-            QuickSort(array, 0, array.Length - 1);
-            PrintArray(array);
-            //CompareSorts(array, 0, array.Length - 1);
+            int[] array = RandomArray(100, 1000000);
+            Sort_CompareTime(array);
+            Console.WriteLine();
+            Sort_CompareSwaps(array);
+            Console.WriteLine();
+            Sort_CompareComparisons(array);
+
+
+
             /*
+
+            Quick sort and Insert sort have the same speed at about 130 elements.
+
 
             250 elements:
             Time elapsed for select sort: 00:00:00.0003182
