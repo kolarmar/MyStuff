e4f524e582f7a6e50ffe57b7c18daf4f320597e2
kolarmar <martin.kolar03@post.cz>
2023-08-03 11:57:59 +0200

    Update DataStructures Rework list.cs to be compatibe with node.cs

--- C#/DataStructures/DataStructures/List.cs
+++ C#/DataStructures/DataStructures/List.cs
@@ -1,80 +1,151 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructures
 {
-    internal class ListNode<T>
+    class List<T> : IEnumerable<T>
     {
-        private T data;
-        private ListNode<T> child;
+        private Node<T> head = null;
+        private Node<T> end = null;
 
-        public ListNode(T _data)
+        private class Enumerator : IEnumerator<T>
         {
-            SetData(_data);
-            this.child = null;
-        }
-
-        public void SetData(T _data) { this.data = _data; }
-
-        public void SetChild(ListNode<T> _child) { this.child = _child; }
-
-        public T GetData() { return this.data; }
-
-        public ListNode<T> GetChild() { return this.child; }
-
-    }
-
-    internal class List<T>
-    {
-        ListNode<T> head;
-
-        public List()
-        {
-            head = null;
-        }
-
-        public void Add(T _data)
-        {
-            if (head == null)
+            List<T> list = null;
+            public Node<T> CurrentNode { get; set; } = null;
+            public Enumerator(List<T> list, Node<T> currentNode = null)
             {
-                head = new ListNode<T>(_data);
+                (this.list, this.CurrentNode) = (list, currentNode);
+            }
+
+            public T Current => CurrentNode.GetData();
+            object IEnumerator.Current => CurrentNode.GetData();
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                if (list.Empty || Last)
+                {
+                    return false;
+                }
+                CurrentNode = (CurrentNode == null) ? list.head : CurrentNode.GetChild();
+                return true;
+            }
+
+            public void Reset() => CurrentNode = null;
+            public bool Last => (CurrentNode != null) && (CurrentNode.GetChild() == list.end);
+
+            public override bool Equals(object obj)
+            {
+                var enumerator = obj as Enumerator;
+                return (enumerator != null) && (list == enumerator.list)
+                && (CurrentNode == enumerator.CurrentNode);
+            }
+
+            public static readonly Enumerator ForNull = new Enumerator(null);
+        }
+
+
+        // Construtors
+        public List() => head = end = new Node<T>();
+        public List(IEnumerable<T> collection) : this()
+        {
+            foreach (var item in collection)
+            {
+                AddToEnd(item);
+            }
+        }
+        public List(params T[] array) : this((IEnumerable<T>)array) { }
+
+
+        // Attributes
+        public bool Empty => head == end;
+        public int Size { get; private set; } = 0;
+
+
+        // Methods
+        public void AddToStart(T item)
+        {
+            head = new Node<T>(item, head);
+            Size++;
+        }
+
+        public void AddToEnd(T item)
+        {
+            end.SetData(item);
+            end.SetChild(new Node<T>());
+            end = end.GetChild();
+            Size++;
+        }
+
+        public void ClearFromStart()
+        {
+            if (Empty) throw new InvalidOperationException(
+                "Cannot clear first element when list is empty");
+
+            head = head.GetChild();
+            Size--;
+        }
+
+        public void ClearAll()
+        {
+            head = end;
+            Size = 0;
+        }
+
+        public bool Find(T search, out IEnumerator<T> enumerator)
+        {
+            enumerator = Enumerator.ForNull;
+
+            if (Empty) return false;
+
+            for (var item = head; item != end; item = item.GetChild())
+            {
+                if (item.GetData().Equals(search))
+                {
+                    enumerator = new Enumerator(this, item);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void ChangeValue(IEnumerator<T> pointOnItem, T newValue)
+        {
+            ((Enumerator)pointOnItem).CurrentNode.SetData(newValue);
+        }
+
+        public void PutBehind(IEnumerator<T> pointOnItem, T newValue)
+        {
+            Node<T> item = ((Enumerator)pointOnItem).CurrentNode;
+            item.SetChild(new Node<T>(newValue));
+            Size++;
+        }
+
+        public void ClearNode(IEnumerator<T> pointOnItem)
+        {
+            Node<T> deletedItem = ((Enumerator)pointOnItem).CurrentNode;
+
+            if (deletedItem == head)
+            {
+                head = head.GetChild();
+            }
+            else if (((Enumerator)pointOnItem).Last)
+            {
+                end = deletedItem;
+                end.SetChild(null);
             }
             else
             {
-
-                ListNode<T> prev = GetLast();
-
-                prev.SetChild(new ListNode<T>(_data));
+                deletedItem.SetData(deletedItem.GetChild().GetData());
+                deletedItem.SetChild(deletedItem.GetChild().GetChild());
             }
-        }
-        public T Peak() => GetLast().GetData();
 
-        public bool IsEmpty() => head == null;
-
-        public ListNode<T> GetLast()
-        {
-            ListNode<T> p = head;
-            ListNode<T> prev = null;
-            while (p != null)
-            {
-                prev = p;
-                p = p.GetChild();
-            }
-            return prev;
+            Size--;
         }
 
-        public bool Contains(T _data)
-        {
-            ListNode<T> p = head;
-            while(p != null && !p.GetData().Equals(_data))
-            {
-                p = p.GetChild();
-            }
-            return p != null;
-        }
-
+        public IEnumerator<T> GetEnumerator() => new Enumerator(this);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
