0c81cf0ee33e5e6274aeff5d8525df1898cabc3d
kolarmar <martin.kolar03@post.cz>
2023-08-03 10:25:32 +0200

    Update DataStructures Reorganization and refinement of node.cs The rest of the files are currently not working due to unapplied changes of node.cs.

--- C#/DataStructures/DataStructures/Node.cs
+++ C#/DataStructures/DataStructures/Node.cs
@@ -1,14 +1,71 @@
 ﻿namespace DataStructures
 {
-    internal class Node<T>
+    public abstract class BaseNode<T>
     {
-        public T data;
-        public Node<T> child;
+        protected T data;
 
-        public Node(T _data, Node<T> _child = null)
+        public void SetData(T _data)
         {
             this.data = _data;
+        }
+        
+        public T GetData()
+        {
+            return this.data;
+        }
+    }
+
+    public class Node<T> : BaseNode<T>
+    {
+        private Node<T> child;
+
+        public Node(T _data = default(T), BaseNode<T> _child = null)
+        {
+            SetData(_data);
+            SetChild((Node<T>)_child);
+        }
+
+        public void SetChild(Node<T> _child)
+        {
             this.child = _child;
+        }
+
+        public Node<T> GetChild()
+        {
+            return this.child;
+        }
+    }
+
+    public class BinaryNode<T> : BaseNode<T>
+    {
+        private BinaryNode<T> childLeft;
+        private BinaryNode<T> childRight;
+
+        public BinaryNode(T _data = default(T), BaseNode<T> _childLeft = null, BaseNode<T> _childRight = null)
+        {
+            SetData(_data);
+            SetLeftChild((BinaryNode<T>)_childLeft);
+            SetRightChild((BinaryNode<T>)_childRight);
+        }
+
+        public void SetLeftChild(BinaryNode<T> _child)
+        {
+            this.childLeft = _child;
+        }
+
+        public void SetRightChild(BinaryNode<T> _child)
+        {
+            this.childRight = _child;
+        }
+
+        public BinaryNode<T> GetLeftChild()
+        {
+            return this.childLeft;
+        }
+
+        public BinaryNode<T> GetRightChild()
+        {
+            return this.childRight;
         }
     }
 }
--- C#/DataStructures/DataStructures/Program.cs
+++ C#/DataStructures/DataStructures/Program.cs
@@ -6,22 +6,24 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            BinaryTree<string> tree = new BinaryTree<string>();
-            tree.Add("a");
-            tree.Add("b");
-            tree.Add("c");
-            tree.Add("d");
-            tree.Add("e");
-            tree.Add("f");
+            //BinaryTree<string> tree = new BinaryTree<string>();
+            //tree.Add("a");
+            //tree.Add("b");
+            //tree.Add("c");
+            //tree.Add("d");
+            //tree.Add("e");
+            //tree.Add("f");
 
-            string[] list = tree.DepthFirstValues();
+            //string[] list = tree.DepthFirstValues();
 
-            foreach (string value in list)
-            {
-                Console.WriteLine(value);
-            }
+            //foreach (string value in list)
+            //{
+            //    Console.WriteLine(value);
+            //}
             
-
+            BinaryNode<string> binNode = new BinaryNode<string>("a");
+            Node<string> node = new Node<string>("xd", binNode);
+            Console.WriteLine(node.GetChild());
 
             Console.ReadLine();
         }
